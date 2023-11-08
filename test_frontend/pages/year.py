c295c8cc96d0b0ede509e6a8f99120f025487c6f
Jiwon Shin <109152749+JiwonGigiShin@users.noreply.github.com>
2023-11-08 21:48:34 +0000

    Delete test_frontend directory

--- test_frontend/pages/year.py
+++
@@ -1,9 +0,0 @@
-import dash
-from dash import html
-
-dash.register_page(__name__)
-
-layout = html.Div([
-    html.H2('Wordcloud By Year', style={'textAlign': 'center'}),
-    html.Div('content', style={'textAlign': 'center'}),
-])
